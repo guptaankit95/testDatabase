017bc11352ec1eb85e5c4c49f54a037bb8e96fae
guptaankit95 <hiankit1995@gmail.com>
2020-01-09 15:26:48 +0530

    Update lms.sql
    adding table and one row for testing

--- lms.sql
+++ lms.sql
@@ -1,1 +1,5 @@
 CREATE DATABASE IF NOT EXISTS lms;
+
+create table tbl_user(userid INT NOT NULL, username varchar(45) NOT NULL , password varchar(45) NOT NULL,PRIMARY KEY (userid));
+
+insert into tbl_user values(1,'Ankit','ankit123@');
